eca49c856651430150e05cfbdcffc7c92ed49da9
nana0sei <frimpongnanao@gmail.com>
2023-04-02 19:43:21 +0000

    first commit

---
+++ src/components/carouselComponent.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import girl from "../images/girl.JPG";
+import girl1 from "../images/girl1.PNG";
+import girl2 from "../images/girl2.PNG";
+import jah from "../images/jah.PNG";
+import od from "../images/od.png";
+
+class CarouselComponent extends Component {
+  state = {};
+  render() {
+    return (
+      <Carousel
+        width="550px"
+        dynamicHeight={true}
+        autoPlay
+        interval="5000"
+        infiniteLoop
+        stopOnHover={false}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+        emulateTouch={false}
+        showArrows={false}
+        useKeyboardArrows={true}
+        animationHandler={"fade"}
+        transitionTime="1500"
+      >
+        <div>
+          <img src={girl} alt=" " className="rounded" />
+        </div>
+        <div>
+          <img src={girl2} alt=" " className="rounded" />
+          {/* <p className="legend">Legend 1</p> */}
+        </div>
+        <div>
+          <img src={od} alt=" " className="rounded" />
+        </div>
+        <div>
+          <img src={girl1} alt=" " className="rounded" />
+        </div>
+        <div>
+          <img src={jah} alt=" " className="rounded" />
+        </div>
+      </Carousel>
+    );
+  }
+}
+
+export default CarouselComponent;
---
+++ src/components/projects.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+import ritalucia from "../images/ritalucia.PNG";
+import smc from "../images/smc.JPG";
+import mayasweb from "../images/mayasweb.PNG";
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="font-face-fl">
+          <p>
+            These are some of the projects I have worked on, with commentary on
+            the processes behind them.
+          </p>
+
+          <div className="row justify-content-center " id="projects">
+            <div className="col-4">
+              <div className="card" style={{ width: 400 }}>
+                <img className="card-img-top" src={ritalucia} alt=" " />
+                <div className="card-body">
+                  <h5 className="card-title font-face-fh">
+                    Lady Legasus Emotes
+                  </h5>
+                  <p className="card-text">
+                    <i>Emote set for a Twitch streamer</i>
+                  </p>
+                  <a href="/ladyleg" className="btn btn-primary">
+                    See details
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4 ">
+              <div className="card" style={{ width: 400 }}>
+                <img className="card-img-top" src={mayasweb} alt=" " />
+                <div className="card-body">
+                  <h5 className="card-title font-face-fh">Maya's Web</h5>
+                  <p className="card-text">
+                    <i>Art direction for an EP</i>
+                  </p>
+                  <a href="/mayasweb" className="btn btn-primary">
+                    See details
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-4">
+              <div className="card" style={{ width: 400 }}>
+                <img className="card-img-top" src={smc} alt=" " />
+                <div className="card-body">
+                  <h5 className="card-title font-face-fh">
+                    Sony Music Camp Merch
+                  </h5>
+                  <p className="card-text">
+                    <i>Merch for Sony Music Recording Camp</i>
+                  </p>
+                  <a href="/smc" className="btn btn-primary">
+                    See details
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Projects;

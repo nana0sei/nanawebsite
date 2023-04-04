41890779d6da97e237de55b8389654cc35db4d39
nana0sei <frimpongnanao@gmail.com>
2023-04-04 10:17:56 +0000

    modified App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import ScrollToTop from "react-scroll-to-top";
-import Projects from "./components/projects";
 import Home from "./components/home";
-import Contact from "./components/contact";
 import Footer from "./components/footer";
 import LadyLeg from "./components/ladyLeg";
 import Smc from "./components/smc";
@@ -26,8 +24,6 @@ class App extends Component {
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
             <Route path="/ladyleg" element={<LadyLeg />} />
             <Route path="/smc" element={<Smc />} />
             <Route path="/mayasweb" element={<MayasWeb />} />
